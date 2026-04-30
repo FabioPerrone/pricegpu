2a87b209cd5cb81f0ce1cbb5b6bb2697f3a5fa93
Fabio <fp.fabioperrone@gmail.com>
2026-04-30 10:31:26 +0200

    Add remaining provider scraper stubs

---
+++ scripts/scrape/tensordock.ts
@@ -0,0 +1,9 @@
+import { saveProviderPrices, PriceItem, withRetry } from "./_shared.js";
+const PROVIDER_SLUG = "tensordock";
+export async function scrape(): Promise<PriceItem[]> {
+  return withRetry(async () => {
+    console.log(`[${PROVIDER_SLUG}] Scraper stub`);
+    return [];
+  });
+}
+if (import.meta.main) scrape().then(items => saveProviderPrices(PROVIDER_SLUG, items)).catch(console.error);
